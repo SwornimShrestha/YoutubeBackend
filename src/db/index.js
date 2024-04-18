12e20f9f101deb72e34ba9ed871858e71ca7c96e
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-04-18 13:20:55 +0545

    setup

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstanace = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST:${connectionInstanace.connection.host}`
+    );
+  } catch (error) {
+    console.log("ERROR:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
