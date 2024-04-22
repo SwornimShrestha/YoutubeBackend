3a11d07d57a006c7d548e8f825d216f249ca2abe
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-04-22 18:44:58 +0545

    all setup config

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_OPTION,
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
@@ -15,4 +15,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+//we cannot declare route like app.use like we normally do we need to use middlewares
+app.use("/api/v1/users", userRouter);
 export { app };
