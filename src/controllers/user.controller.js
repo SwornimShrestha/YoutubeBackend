933baacc526be3e706281ecc5a510b88a14b0c94
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-05-01 22:32:18 +0545

    add accessrefreshtoken endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,20 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudnary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
+const generateAccessAndRefereshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefereshToken();
+
+    user.refreshToken = refreshToken; //saving in database
+    user.save({ validateBeforeSave: false }); //other mongooose will kickin if we save refereshtoken so use this
+
+    return { accessToken, refreshToken };
+  } catch (error) {}
+};
 const registerUser = asyncHandler(async (req, res) => {
   //get user detail from frontend
   //validation -not empty
@@ -40,12 +53,14 @@ const registerUser = asyncHandler(async (req, res) => {
   const avatarLocalPath = req.files?.avatar[0]?.path;
   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-
   let coverImageLocalPath;
   // show if array have come or not
-  if (req.files && Array.isArray(req.files.coverImage) &&req.files.coverImage.length >0) {
-    coverImageLocalPath == req.files.coverImage[0].path
-    
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath == req.files.coverImage[0].path;
   }
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
@@ -84,4 +99,135 @@ const registerUser = asyncHandler(async (req, res) => {
   // });
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  //req-body=data
+  // username or email
+  // find the user
+  // password check
+  // access token and referesh token
+  // send cookie
+
+  const { email, username, password } = req.body;
+
+  if (!username && !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exists");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  //without this user can easily modify front frontend
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+
+  const logoutUser = asyncHandler(async (req, res) => {});
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { refreshToken: undefined },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+const refereshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefereshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+  if (incomingRefereshToken) {
+    throw new ApiError(401, "unauthorized request  ");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefereshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id);
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Referesh Token  ");
+    }
+
+    if (incomingRefereshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Referesh Token is expired or used");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefereshTokens(user._id);
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refereshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access Token Refreshed"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token");
+  }
+});
+
+export { registerUser, loginUser, logoutUser, refereshAccessToken };
